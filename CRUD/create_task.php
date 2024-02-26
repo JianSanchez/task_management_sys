e49a2821c74d8687ddf1b462ef20cf845926ab70
jakkusu1 <olango.jabezjames@gmail.com>
2024-02-26 19:55:42 +0800

    added create and update php scripts

---
+++ CRUD/create_task.php
@@ -0,0 +1,21 @@
+<?php
+include("config.php");
+
+if (isset($_POST[""])){
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $priority = $_POST["priority"];
+    $duedate = $_POST["duedate"];
+
+    $sql = "INSERT INTO tasks (title, description, priority, due_date) VALUES ('$title', '$description', '$priority', '$duedate')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Added Task Successfully";
+      } else {
+        echo "Failed to add task";
+        // echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+      
+    $conn->close();
+}
+?>
