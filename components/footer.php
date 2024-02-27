e4261445a455452c4e4f518f2282bd18f5a38d95
JianSanchez <sanchez.jianimmanuel@gmail.com>
2024-02-28 01:11:01 +0800

    added header(buttons(not functional)) and footer simple colors no designs just text and colors and text alignments

--- components/footer.php
+++ components/footer.php
@@ -2,11 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="footer">
-
+    <div class="card-footer bg-warning border border-info">
+        <div class="text-center">
+            <h5>These are the rights that authors are entitled to according to the law of copyright,
+                under Part IV of R.A. 8293, or the Intellectual Property Code of the Philippines.</h5>
+            <h5>We Company Inc.</h5>
+            <h5>Call Us 0987652211</h5>
+            <h5>Message us: WeCompany@gmail.com</h5>
+            <h5>All rights Reserve</h5>
+        </div>
     </div>
 </body>
 </html>
--- components/header.php
+++ components/header.php
@@ -5,9 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <div class="container bg-warning d-flex">
-        <p>hello</p>
+<body class="bg-secondary">
+    <div class="navbar navbar-expand-lg bg-warning border border-info">
+        <h3>Task Management System</h3>
+        <div class="container d-flex justify-content-end">
+            <div>
+                <button class="rounded-3" name="add_Task">Add Task</button>
+                <button class="rounded-3" name="edit_Task">Edit Task</button>
+            </div>
+            
+        </div>
+        
     </div>
 </body>
 </html>
